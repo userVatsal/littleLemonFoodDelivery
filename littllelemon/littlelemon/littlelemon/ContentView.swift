81cb4e7b0a0a079e69aae7c72df036ec020bc226
Vatsal Pareshkumar <127995002+userVatsal@users.noreply.github.com>
2023-07-18 18:32:56 +0100

    Create ContentView.swift

---
+++ littllelemon/littlelemon/littlelemon/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  littlelemon
+//
+//  Created by Robert Palmer on 6/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            HStack{
+                
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
